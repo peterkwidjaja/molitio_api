707824e644f8c807f159356486c8b72987df5bef
Peter Kristianto Widjaja <peterkwidjaja@gmail.com>
2015-10-28 17:04:39 +0800

    init commit

---
+++ app.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+require 'rubygems'
+require 'json'
+
+configure do
+
+
+get '/' do
+  {
+    'username'=> 'lol',
+    'password'=> 'test'
+  }.to_json
+end
+
+post '/auth' do
+  username = params[:username]
+  password = params[:password]
+  {
+    'secret_key'=>'1234512345'
+  }.to_json
+end
+
+#REGISTER FROM MOBILE PHONE
+get '/register' do
+  {'message'=>'Successful'}.to_json
+end
+
+post '/register' do
+  username = params[:username]
+  password = params[:password]
+  status 200
+  {
+    'message' => 'Successful'
+  }.to_json
+end
+
+
