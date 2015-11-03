3afb827752247488b2568744e30b29347b910ff3
Peter Kristianto Widjaja <peterkwidjaja@gmail.com>
2015-11-03 15:49:44 +0800

    use authentication

--- app.rb
+++ app.rb
@@ -14,22 +14,17 @@ end
 post '/auth' do
   username = params[:username]
   password = params[:password]
-#  if(username == "test" && password == "pass1234")
-#    {
-#      'id'=>1,
-#      'auth_token'=>'1234512345'
-#    }.to_json
-#  else
-#    status 401
-#    {
-#      'message'=>'Wrong username and password combination'
-#    }.to_json
-#  end
-
-{
-  'id'=>1,
-  'auth_token'=>'1234512345'
-}.to_json
+  if(username == "test@mail.com" && password == "pass1234")
+    {
+      'id'=>1,
+      'auth_token'=>'1234512345'
+    }.to_json
+  else
+    status 401
+    {
+      'message'=>'Wrong username and password combination'
+    }.to_json
+  end
 end
 
 #REGISTER FROM MOBILE PHONE
